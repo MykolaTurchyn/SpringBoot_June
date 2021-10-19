d591eca689f2a0a46ade3536057157df1b7d2117
mykolaturchyn <tyrchnmikigt@gmail.com>
2021-10-19 19:17:22 +0300

    Initial commit

---
+++ src/main/java/com/example/springboot_june/SpringBootJuneApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_june;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJuneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJuneApplication.class, args);
+    }
+
+}
